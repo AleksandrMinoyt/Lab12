794af70db935c05712178cbc7a83da146bf4f401
fox_itleksandr <fox_it@bk.ru>
2022-01-13 10:38:15 +0300

    Add project files.

---
+++ Lab12/CircleMetod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab12
+{
+    public static class CircleMetod
+    {
+        static public double Length(double radius)
+        {
+            double result = 2 * Math.PI * radius;
+            return result;
+        }
+        static public double Square(double radius)
+        {
+            double result = Math.PI * Math.Pow(radius, 2);
+            return result;
+        }
+
+        static public bool CheckEntry(double radius, double coord_x, double coord_y)
+        {
+            bool result = (Math.Sqrt(Math.Pow(coord_x, 2) + Math.Pow(coord_y, 2))) < radius;
+            return result;
+        }
+
+
+    }
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка статического класса для работы с окружностью");
+            Console.WriteLine("Выберите операцию:");
+            Console.WriteLine("1 - Длина окружности");
+            Console.WriteLine("2 - Площадь круга");
+            Console.WriteLine("3 - Принадлежность точки кругу");
+            try
+            {
+                int operation = Convert.ToInt32(Console.ReadLine());
+                switch (operation)
+                {
+                    case 1:
+                        CalculateLengthCircle();
+                        break;
+                    case 2:
+                        CalculateSquareCircle();
+                        break;
+                    case 3:
+                        CheckEntry();
+                        break;
+                    default: throw new Exception("Не правильный выбор операции!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка!!! " + ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+
+        public static void CalculateLengthCircle()
+        {
+            Console.WriteLine("Расчет длины окружности по радиусу");
+            Console.WriteLine("Введите радиус");
+            try
+            {
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Периметр={0}", CircleMetod.Length(radius));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка!!! " + ex.Message);
+            }
+        }
+        public static void CalculateSquareCircle()
+        {
+            Console.WriteLine("Расчет площади круга по радиусу");
+            Console.WriteLine("Введите радиус");
+            try
+            {
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Площадь={0}", CircleMetod.Square(radius));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка!!! " + ex.Message);
+            }
+        }
+        public static void CheckEntry()
+        {
+            Console.WriteLine("Проверка принадлежности точки кругу по радиусу");           
+            try
+            {
+                Console.WriteLine("Введите радиус");
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату x");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Точка {0} кругу",CircleMetod.CheckEntry(radius,x,y)?"принадлежит":"НЕ принадлежит" );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка!!! " + ex.Message);
+            }
+        }
+
+
+    }
+
+}
